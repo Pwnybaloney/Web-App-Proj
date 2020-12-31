eba65bb5d617ddb22a374c733a6eb0122bc76415
Pwnybaloney <cuitony.baloney@gmail.com>
2020-12-30 22:05:31 -0800

    Update index.js
    Testing out Github Desktop

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//Jacky has huge thighgs
 //Irene is swole
 // class Square extends React.Component {
 //     constructor(props) {
